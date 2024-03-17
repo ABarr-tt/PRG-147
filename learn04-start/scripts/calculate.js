8dfe68482854d4f81c4f0c49a88752987cc22090
Aidan Barrett <142757349+ABarr-tt@users.noreply.github.com>
2024-03-17 12:11:30 -0500

    Add files via upload

---
+++ learn04-start/scripts/calculate.js
@@ -0,0 +1,22 @@
+function addDate(){
+    let today = new Date();
+    document.getElementById("mydate").innerHTML = today;
+    document.getElementById("name").value = "";
+    document.getElementById("rm1length").value = "";
+    document.getElementById("rm1width").value = "";
+    document.getElementById("rm1height").value = "";
+    document.getElementById("rm1cost").value = "";
+}
+
+function estimate(){
+    let name = document.getElementById("name").value
+    let length = parseFloat(document.getElementById("rm1length").value);
+    let width = parseFloat(document.getElementById("rm1width").value);
+    let height = parseFloat(document.getElementById("rm1height").value);
+    let totalSqFt = ((length * height * 2) + (length * width * 2));
+    let cost = (totalSqFt * .65);
+    document.getElementById("rm1cost").value = cost;
+
+    var totalCost = cost;
+    document.getElementById("estimate").innerHTML = (name + ", your estimate is: " + totalCost)
+}
