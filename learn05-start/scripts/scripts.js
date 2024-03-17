64a3af5f228b0655553e2d9a16660411b9bdcd09
Aidan Barrett <142757349+ABarr-tt@users.noreply.github.com>
2024-03-17 12:44:32 -0500

    Add files via upload

---
+++ learn05-start/scripts/scripts.js
@@ -0,0 +1,18 @@
+const dog1 = {
+    heading: "Bucket Case",
+    image: "images/black-dog.jpg",
+    caption: "Just hanging out waiting for water",
+    altTag: "Black dog in bucket."
+   };
+   function chooseDog(dog) {
+    if (dog == 1) {
+    loadMe(dog1, 1) // don't put dog1 in quotes or it will pass a string not the object
+    }
+   }
+   function loadMe(dog, dogNum) {
+    document.getElementById("heading" + dogNum).innerHTML = dog.heading;
+    document.getElementById("img" + dogNum).src = dog.image;
+    document.getElementById("img" + dogNum).style.display = "block";
+    document.getElementById("p" + dogNum).innerHTML = dog.caption;
+    document.getElementById("img" + dogNum).setAttribute('alt', dog.altTag);
+   }
